d44c2ba7c5f2a3b8de689ecd97a9d9a08f62b940
somq14 <som.q14@gmail.com>
2021-02-17 19:57:46 +0900

    hello api

---
+++ src/app.js
@@ -0,0 +1,15 @@
+exports.lambdaHandler = async (event, context) => {
+    let response
+    try {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+    return response
+};
